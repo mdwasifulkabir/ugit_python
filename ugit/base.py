a6c6acddbc833193f36bd874d1d61e9775b1387a
mdwasifulkabir <wasifulkabir9016@gmail.com>
2026-05-15 20:03:31 +0800

    Create get_commit function:

--- ugit/base.py
+++ ugit/base.py
@@ -1,4 +1,8 @@
 import os
+import itertools
+import operator
+
+from collections import namedtuple
 
 from . import data
 
@@ -102,6 +106,25 @@ def commit(message):
   data.set_HEAD(oid)
 
   return oid
+
+Commit = namedtuple('Commit', ['tree', 'parent'])
+
+def get_commit(oid):
+  parent = None
+
+  commit = data.get_object(oid, 'commit').decode()
+  lines = iter(commit.splitlines())
+  for line in itertools.takewhile(operator.truth, lines):
+    key, value = line.split('', 1)
+    if key == 'tree':
+      tree = value
+    elif key == 'parent':
+      parent = value
+    else:
+      assert False, f'Unknown field {key}'
+
+  message = '\n'.join(lines)
+  return Commit(tree=tree, parent=parent, message=message)
 
 def is_ignored(path):
   parts = path.split('/')
