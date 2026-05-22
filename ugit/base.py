e8e4f6261fdde2ed5b8775a2880b8794962ee1b7
mdwasifulkabir <wasifulkabir9016@gmail.com>
2026-05-22 18:37:56 +0800

    Add default argument to be @(alias for HEAD)

--- ugit/base.py
+++ ugit/base.py
@@ -134,6 +134,8 @@ def get_commit(oid):
   return Commit(tree=tree, parent=parent, message=message)
 
 def get_oid(name):
+  if name == '@': name = 'HEAD'
+
   #If name is ref
   refs_to_try = [
     f'{name}',
